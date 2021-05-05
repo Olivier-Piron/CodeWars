71f1e0a2d02433f67114f9a24590e9513eeacd9f
Olivier Piron <76862461+Olivier-BeCode@users.noreply.github.com>
2021-05-05 20:01:09 +0200

    Update TenMinuteWalk.py

--- TenMinuteWalk.py
+++ TenMinuteWalk.py
@@ -14,3 +14,8 @@ def is_valid_walk(walk):
             if i == "w": nW += 1 
             if i == "e": nE += 1
         return True if nN == nS and nW == nE else False
+    
+###
+
+def isValidWalk(walk):
+    return len(walk) == 10 and walk.count('w') == walk.count('e') and walk.count('n') == walk.count('s')
