acda19ab1b5e216018ae42c5e66ecabb2133f666
Olivier Piron <76862461+Olivier-BeCode@users.noreply.github.com>
2021-05-06 12:51:24 +0200

    Create ValidParentheses.py

---
+++ ValidParentheses.py
@@ -0,0 +1,9 @@
+# Valid Parentheses
+
+def valid_parentheses(string):
+    count = 0
+    for i in string:
+        if i == '(': count += 1
+        if i == ')': count -= 1
+        if count < 0: return False
+    return count == 0
