90f18e9c49c48ef4e71340c1983c3fa21e80d933
Olivier-BeCode <76862461+Olivier-BeCode@users.noreply.github.com>
2021-06-04 16:14:42 +0200

    Edit

--- Python/4kyu/NestingStructuresComparaison.py
+++ Python/4kyu/NestingStructuresComparaison.py
@@ -1,32 +1,14 @@
 # Nesting Structure Comparison
 
 def same_structure_as(original,other):
-    lenghts1 = []
-    lenghts2 = []
-
-    if isinstance(original, list) == True and isinstance(other, list) == True:
-        if original == [1,'[',']'] and other == ['[',']',1]:
-            return True
-        else:
-            for cells in original:
-                if isinstance(cells, list) == True:
-                    lenghts1.append(len(cells))
-                    for i in cells:
-                        if isinstance(i, list) == True:
-                            lenghts1.append(len(i))
-                else:
-                    lenghts1.append(type(cells))
-            for cells in other:
-                if isinstance(cells, list) == True:
-                    lenghts2.append(len(cells))
-                    for i in cells:
-                        if isinstance(i, list) == True:
-                            lenghts2.append(len(i))
-                else:
-                    lenghts2.append(type(cells))
-            if lenghts1 != lenghts2:
-                return False
-            else:
-                return True
-    else:
+    if isinstance(original, list) != isinstance(other, list):
         return False
+    elif isinstance(original, list):
+        if len(original) != len(other):
+            return False
+        for i in range(len(original)):
+            if not same_structure_as(original[i], other[i]):
+                return False
+        return True
+    else:
+        return True
